0750c4c6c4dcdb9d00d23ccd6c531738ec20330e
Emmeric Royer <emmeric.royer@supinternet.fr>
2014-01-14 10:53:58 +0100

    debut routing plus 1ere version arbo

---
+++ core/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once("../vendors/spyc-master/spyc.php");
+require_once("./models/Routing.php");
+
+$routing = new Routing\Routing();
---
+++ core/models/Routing.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Routing;
+
+class Routing {
+
+   private $routes;
+
+   public function __construct(){
+       $routing = './../config/routing.yml';
+       if (!file_exists($routing)){
+           throw new \Exception("Unknown routing file");
+       }
+       $this->routes = \Spyc::YAMLLoad($routing);
+       var_dump($this->routes);
+   }
+
+   public function getController(){
+
+   }
+} 
